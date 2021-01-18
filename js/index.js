8ed180c3cbac29e3398203e3e8113d44a6406a1d
Shenica Graham <gshenica@gmail.com>
2021-01-17 23:29:17 -0600

    Add mousedown, mouseup, and mousemove events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// Events 1(mousedown), 2(mouseup), and 3(mousemove)
+
+    // Grab the page header.
+    let header = document.querySelector("header");
+
+    // Event 1 -  mousedown on header.
+    header.addEventListener('mousedown', () => {
+        header.style.background = "#000000"});
+    // Event 2 -  mouseup on header.
+    header.addEventListener('mouseup', () => {
+        header.style.background = '#ffffff'});
+    // Event 3 -  reset background color on mousemove over header.
+    header.addEventListener('mousemove', () => {
+        header.style.background = '#ffffff'});
+
