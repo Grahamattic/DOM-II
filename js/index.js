11363a860963d307ce9b8d7efb2656ef947b6e89
Shenica Graham <gshenica@gmail.com>
2021-01-17 23:31:42 -0600

    Rotate h1 on double click

--- js/index.js
+++ js/index.js
@@ -10,8 +10,21 @@
         header.style.background = "#000000"});
     // Event 2 -  mouseup on header.
     header.addEventListener('mouseup', () => {
-        header.style.background = '#ffffff'});
+        header.style.background = '#669900'});
     // Event 3 -  reset background color on mousemove over header.
     header.addEventListener('mousemove', () => {
         header.style.background = '#ffffff'});
+
+
+// Event 4(double click)
+
+    // Grab the h1 heading
+    let h1 = document.querySelector("h1");
+
+    // Event 4 - rotate the h1 heading on double click.
+    h1.addEventListener('dblclick', () => {
+        h1.style.transform = "rotate(360deg)";
+        h1.style.transition = "all 1s";
+    });
+
 
